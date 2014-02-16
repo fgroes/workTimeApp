d82ebbb411e548d7a2f9a52c48675b30c42b1591
friedrich groestlinger <friedrich.groestlinger@gmail.com>
2014-02-16 23:12:35 +0100

    basic widget with calendar and week table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from database import *
-from PySide import QtGui
+from tableModel import *
+from PySide import QtGui, QtCore
 import sys
 
 
@@ -8,10 +8,11 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
-        self.show()
+        self.show()      
 
     def initUI(self):
         self.setWindowTitle('basic window')
+        self.setGeometry(100, 100, 1100, 900)
         self.menu = self.menuBar()
         self.fileMenu = self.menu.addMenu('&File')
         exitAction = QtGui.QAction('&Exit', self)
@@ -19,22 +20,37 @@ class MainWindow(QtGui.QMainWindow):
         centralWidget = QtGui.QWidget()
         self.fileMenu.addAction(exitAction)
         self.centralLayout = QtGui.QVBoxLayout()
-        self.centralLayout.addWidget(QtGui.QPushButton('Test'))
-        self.tableView = QtGui.QTableView()
-        self.setTable()
-        self.tableView.setModel(self.tableModel)
+        self.calendar = QtGui.QCalendarWidget(parent=self)
+        self.calendar.setFirstDayOfWeek(QtCore.Qt.Monday)
+        self.centralLayout.addWidget(self.calendar)
+        self.tableView = QtGui.QTableView()   
+        #self.setTable()
+        self.calendar.clicked.connect(self.setCurrentDate)
+        self.setCurrentDate(QtCore.QDate.currentDate())
         self.centralLayout.addWidget(self.tableView)
         centralWidget.setLayout(self.centralLayout)
         self.setCentralWidget(centralWidget)
-
-    def setTable(self):
-        table = Table(['Name', 'Age'])
-        table.addRow(['fritz', '31'])
-        table.addRow(['dominik', '28'])
-        table.addRow(['andi', '26'])
-        table.addRow(['franz', '34'])
+    
+    def setCurrentDate(self, currentDate):      
+        dateMonday = currentDate.addDays(1 - currentDate.dayOfWeek())
+        datesOfWeek = []
+        daysOfWeek = QtCore.Qt.DayOfWeek.values.keys()
+        numberOfDaysInWeek = len(daysOfWeek)
+        for i in range(numberOfDaysInWeek):
+            date = dateMonday.addDays(i)
+            datesOfWeek.append(date.toString('ddd dd.MM.yy'))
+        table = Table(datesOfWeek, [])
+        for hour in range(0, 24):
+            for minute in range(0, 60, 30):
+                time = '{0:02d}:{1:02d}'.format(hour, minute)
+                table.addRow(
+                    ['' for i in range(numberOfDaysInWeek)],
+                    time)
         self.tableModel = TableModel()
         self.tableModel.setTable(table)
+        self.tableView.setModel(self.tableModel)
+        for i in range(self.tableModel.columnCount()):
+            self.tableView.setColumnWidth(i, 200)
 
 
 def main():
--- tableModel.py
+++ tableModel.py
@@ -3,61 +3,77 @@ from PySide import QtCore
 
 class TableModel(QtCore.QAbstractTableModel):
 
-	def __init__(self, parent=None, *args):
-		super(TableModel, self).__init__(parent, *args)
-		
-	def setTable(self, table):
-		self.table = table
+    def __init__(self, parent=None, *args):
+        super(TableModel, self).__init__(parent, *args)
+        
+    def setTable(self, table):
+        self.table = table
 
-	def rowCount(self, parent):
-		return self.table.rowCount
+    def rowCount(self, parent=None):
+        return self.table.rowCount
 
-	def columnCount(self, parent):
-		return self.table.columnCount
+    def columnCount(self, parent=None):
+        return self.table.columnCount
 
-	def data(self, index, role):
-		if not index.isValid():
-			return None
-		elif role != QtCore.Qt.DisplayRole:
-			return None
-		return self.table.getData(index.row(), index.column())
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        elif role != QtCore.Qt.DisplayRole:
+            return None
+        return self.table.getData(index.row(), index.column())
 
-	def headerData(self, index, orientation, role):
-		if role != QtCore.Qt.DisplayRole:
-			return None
-		result = None
-		if (orientation == QtCore.Qt.Horizontal):
-			result = self.table.getHeader(index)
-		elif (orientation == QtCore.Qt.Vertical):
-			result = index + 1
-		return result
+    def headerData(self, index, orientation, role):
+        if role != QtCore.Qt.DisplayRole:
+            return None
+        result = None
+        if (orientation == QtCore.Qt.Horizontal):
+            header = self.table.getHorizontalHeader(index)
+            if header is not None:
+                result = header
+            else:
+                result = index + 1
+        elif (orientation == QtCore.Qt.Vertical):
+            header = self.table.getVerticalHeader(index)
+            if header is not None:
+                result = header
+            else:
+                result = index + 1
+        return result
 
-	
+    
 class Table(object):
 
-	def __init__(self, header):
-		self.header = header
-		self.data = []
+    def __init__(self, horizontalHeader=None, verticalHeader=None):
+        self.horizontalHeader = horizontalHeader
+        self.verticalHeader = verticalHeader
+        self.data = []
 
-	def addRow(self, row):
-		if (len(row) == len(self.header)):
-			self.data.append(row)
+    def addRow(self, row, header=None):
+        if (len(row) == len(self.horizontalHeader)):
+            self.data.append(row)
+            if header is not None and self.verticalHeader is not None:
+                self.verticalHeader.append(header)
 
-	def getData(self, row, column):
-		return self.data[row][column]
+    def getData(self, row, column):
+        return self.data[row][column]
 
-	def getHeader(self, column):
-		return self.header[column]
+    def getHorizontalHeader(self, column):
+        header = self.horizontalHeader
+        return header[column] if header is not None else None
+        
+    def getVerticalHeader(self, row):
+        header = self.verticalHeader
+        return header[row] if header is not None else None
 
-	def getColumnCount(self):
-		return len(self.header)
+    def getColumnCount(self):
+        return len(self.horizontalHeader)
 
-	columnCount = property(getColumnCount)
+    columnCount = property(getColumnCount)
 
-	def getRowCount(self):
-		return len(self.data)
+    def getRowCount(self):
+        return len(self.data)
 
-	rowCount = property(getRowCount)
+    rowCount = property(getRowCount)
 
 
 if __name__ == '__main__':
